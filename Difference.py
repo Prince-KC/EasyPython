9dccaa932997dc6b0b47af7f7fb30ed6d5472053
Prince K.C <princekc645@gmail.com>
2025-08-21 06:34:09 +0545

    Product os two numbers is added

--- Difference.py
+++ Difference.py
@@ -2,5 +2,7 @@ a=int(input("enter the first number: "))
 b=int(input("enter the second number: "))
 diff=a-b
 sum=a+b
+product=a*b
 print("difference is",diff)
 print("Sum is",sum)
+print("Product is", product)
